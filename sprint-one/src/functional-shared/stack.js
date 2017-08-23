22350e490892f709471c2b98fb75ce501900b9b5
Hack Reactor Student <student@hackreactor.com>
2017-08-23 16:11:25 -0700

    passing first test on functional-shared

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,22 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  
+  var someMethod = {};
+  
+  _.extend(someMethod, stackMethods);
+  return someMethod;
 };
 
-var stackMethods = {};
+
+var stackMethods = {
+  size: function() {
+    return 0;
+  }
+  
+  
+  
+  
+};
 
 
