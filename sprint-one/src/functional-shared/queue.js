a4b24407f27c63337360a4288dfaa8c4ef013ca2
Hack Reactor Student <student@hackreactor.com>
2017-08-23 16:23:49 -0700

    Write first queue test

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,20 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var sharedMethods = {};
+  sharedMethods.count = 0;
+  
+  _.extend(sharedMethods, queueMethods);
+  
+  return sharedMethods;
+
 };
 
-var queueMethods = {};
+var queueMethods = {
+  
+  size: function() {
+    return this.count;
+  }
+};
 
 
