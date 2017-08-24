95093ab74c79e458018af7f58b2bece13d026f57
Hack Reactor Student <student@hackreactor.com>
2017-08-23 19:54:55 -0700

    passing all shared tests

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,26 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  
+  var makeQueue = Object.create(queueMethods);
+  makeQueue.count = 0;
+  makeQueue.storage = {};
+  return makeQueue;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  size: function() {
+    return this.count;
+  }, 
+  enqueue: function(value) {
+    this.storage[this.count] = value;
+    this.count++;
+  }, 
+  dequeue: function() {
+    if (this.count > 0) {
+      this.count--;
+      return this.storage[this.count];
+    }
+  }};
 
 
