7adc13a150c6d60b97acac835a2d8ab00c6cef30
Hack Reactor Student <student@hackreactor.com>
2017-08-24 10:28:52 -0700

    Pass first 4 tests of linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,9 +4,22 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if (this.tail) {
+      this.tail.next = newNode;
+      if (!this.head) {
+        this.head = this.tail;
+      }
+      this.tail = newNode;
+    } else {
+      this.tail = Node(value);
+    }
   };
 
   list.removeHead = function() {
+    var oldHead = this.head;
+    this.head = this.head.next;
+    return oldHead;
   };
 
   list.contains = function(target) {
