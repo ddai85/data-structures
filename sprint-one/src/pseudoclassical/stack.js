7bb32d6e8a6bc6f27d0a2e31fc0f17ddfa7ac45b
Hack Reactor Student <student@hackreactor.com>
2017-08-23 20:24:28 -0700

    all stack tests passing

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -2,10 +2,10 @@ var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   
-  var makeStacks = Object.create(Stack.prototype);
-  makeStacks.count = 0;
-  makeStacks.storage = {};
-  return makeStacks;
+  
+  this.count = 0;
+  this.storage = {};
+  
 };
 
 Stack.prototype.size = function() {
