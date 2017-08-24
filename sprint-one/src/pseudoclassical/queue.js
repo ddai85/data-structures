30ae2d37138cd5029565b6815d7ec24ae2d1d91d
Hack Reactor Student <student@hackreactor.com>
2017-08-23 20:26:35 -0700

    all but last two tests passing

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,23 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  
+  this.count = 0;
+  this.storage = {};
+};
+
+Queue.prototype.size = function() {
+  return this.count;
+};
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.count] = value;
+  this.count++;
+};
+Queue.prototype.dequeue = function() {
+  if (this.count > 0) {
+    this.count--;
+    return this.storage[this.count];
+  }
 };
 
 
