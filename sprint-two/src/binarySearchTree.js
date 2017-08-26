99fc7bc06bbab2e817dbfc300519aaa647321acb
Hack Reactor Student <student@hackreactor.com>
2017-08-25 19:45:09 -0700

    Write contains function

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -39,10 +39,39 @@ BinarySearchTree.prototype.insert = function(value) {
 };
 
 BinarySearchTree.prototype.contains = function(target) {
-    
+  //  recursive search function that searches nodes
+  var search = function(node) {
+    // if node.value === target
+
+    if (node.value === target) {
+      //  return true;
+      return true;
+    } else {
+      //  compare target with node value
+      if (target < node.value) {
+        // recurse down left if target is less, right if greater
+        if (node.left) {
+          return search(node.left);
+        } else {
+          return false;
+        }
+      } else {
+        if (node.right) {
+          return search(node.right);
+        } else {
+          //  if branch we want is undefined
+          //  return false;
+          return false;
+        }
+      }
+    }
+  };
+  return search(this);
 };
 
 BinarySearchTree.prototype.depthFirstLog = function() {
+  
+  //var 
     
 };
 
